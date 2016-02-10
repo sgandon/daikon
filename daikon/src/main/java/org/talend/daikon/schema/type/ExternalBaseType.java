65451540ec373ac30e25626ad3285d6e0109017f
Ryan Skraba <rskraba@talend.com>
2016-02-10 18:48:42 +0100

    Move ExternalBaseType to an interface.

--- daikon/src/main/java/org/talend/daikon/schema/type/ExternalBaseType.java
+++ daikon/src/main/java/org/talend/daikon/schema/type/ExternalBaseType.java
@@ -1,23 +1,19 @@
 package org.talend.daikon.schema.type;
 
 /**
- * Interface for callbacks thatconvert between application-specific (external) types and known types, including reading
+ * Interface for callbacks that convert between application-specific (external) types and known types, including reading
  * and writing from container objects.
  * 
  * No component is required to know the application-specific types of another component, but all components should be
  * able to manage known types.
  */
-public abstract class ExternalBaseType<SpecificT, KnownT, ReadContainerT, WriteContainerT> {
+public interface ExternalBaseType<SpecificT, KnownT, ReadContainerT, WriteContainerT> {
 
-    public abstract SpecificT readValue(ReadContainerT obj, String key);
+    public SpecificT readValue(ReadContainerT obj, String key);
 
-    public abstract void writeValue(WriteContainerT app, String key, SpecificT value);
+    public void writeValue(WriteContainerT app, String key, SpecificT value);
 
-    public abstract SpecificT convertFromKnown(KnownT value);
+    public SpecificT convertFromKnown(KnownT value);
 
-    public abstract KnownT convertToKnown(SpecificT value);
-
-//    public final void assign2AValue(KnownT value, WriteContainerT app, String key) {
-//        writeValue(app, key, convertFromKnown(value));
-//    }
+    public KnownT convertToKnown(SpecificT value);
 }
